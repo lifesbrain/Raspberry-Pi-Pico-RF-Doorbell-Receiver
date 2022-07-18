18871eae1e2f794c1761f6943e788f2e01f11fbf
Michael Ritchie <michael@ritchie.me>
2022-07-18 01:28:18 +0100

    Libarys impoted, Bell class set

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 import os as uos
+import time
+from array import array
+from utime import ticks_us, ticks_diff
+import ujson
+
+from machine import Pin
 from wavePlayer import wavePlayer
 
-def playAudio():
-    player = wavePlayer()
-    wavName= "haiya"
-    try:
-        player.play(f"/audio/{wavName}.wav")
-    except KeyboardInterrupt:
+class Bell():
+    def __init__(self, code="code"):
+        self._code = {}
+        self.loadCode(f"{code}.json")
+
+    def __call__(self): # Runs doorbell
+        pass
+
+    def loadCode(self, jsonFile):  # Import json code file
+        try:
+            with open(jsonFile, 'r') as f:
+                self._code.update(ujson.load(f))
+        except OSError:
+            print(f"Can't open {self._code} for reading.")
+
+    def listenRF(self): # Listens for saved code on RF
+        pass
+
+    def playAudio():
+        player = wavePlayer()
+        wavName= "haiya"
+        try:
+            player.play(f"/audio/{wavName}.wav")
+        except KeyboardInterrupt:
+            player.stop()
         player.stop()
-    player.stop()
+
+    def readVol():
+        pin = Pin(17, Pin.IN, Pin.PULL_UP)
+        print(f"Checking pin for change")
+        currentPinVal = pin.value()
+        print(f"Pin = {currentPinVal}")
+        while True:
+            newPinVal = pin.value()
+            if newPinVal != currentPinVal:
+                print(f"Pin = {newPinVal}")
+                currentPinVal = newPinVal
+
 
 if __name__ == "__main__":
     pass
     #playAudio()
+
+    #Import RX, 
+    #constantly sample data, compare low/highs against json,
+    #if match, pause sampling and play audio, inform others
+    #return to samping
